96fd09c455946d143a711b93aa8b62d232c94ccf
DHOMD <DHO@dho.im>
2016-12-10 10:38:19 -0500

    Fixup Mis Merge
    Change-Id: I31f80e5a01165f9949976d75982d1765084ec020

--- extract-files.sh
+++ extract-files.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-<<<<<<< HEAD
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$ `; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
-=======
 export VENDOR=lge
 export DEVICE=d802
 ./../../$VENDOR/g2-common/extract-files.sh $@
->>>>>>> cm/cm-12.1
